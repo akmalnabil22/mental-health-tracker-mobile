cc47b1ad8ec8663ab2a6ac869592fcde802a5777
akmalnabil22 <akmaln761@gmail.com>
2024-11-13 15:22:14 +0700

    tutor 8 selesai

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:mental_health_tracker/screens/menu.dart';
 import 'package:mental_health_tracker/screens/moodentry_form.dart';
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -50,6 +50,7 @@ class LeftDrawer extends StatelessWidget {
                       builder: (context) => MyHomePage(),
                     ));
               },
+              
             ),
             ListTile(
               leading: const Icon(Icons.mood),
@@ -62,6 +63,17 @@ class LeftDrawer extends StatelessWidget {
                     builder: (context) => MoodEntryFormPage(),
                     ));
               },
+            ),
+            ListTile(
+                leading: const Icon(Icons.add_reaction_rounded),
+                title: const Text('Daftar Mood'),
+                onTap: () {
+                    // Route menu ke halaman mood
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const MoodEntryPage()),
+                    );
+                },
             ),
         ],
       ),
